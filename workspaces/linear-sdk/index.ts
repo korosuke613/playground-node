b92ee943990c8c01aeb0a5dd8cd9142b092abd8b
Futa Hirakoba <futa-hirakoba@cybozu.co.jp>
2021-03-06 16:40:15 +0900

    update

--- workspaces/linear-sdk/index.ts
+++ workspaces/linear-sdk/index.ts
@@ -1,13 +1,18 @@
-import { LinearClient, LinearFetch, User } from "@linear/sdk";
+import { LinearClient } from "@linear/sdk";
 
-const apiKey = process.env.LINEAR_API_KEY
+const apiKey = process.env.LINEAR_API_KEY;
 const linearClient = new LinearClient({ apiKey });
 
-async function getCurrentUser(){
-    return linearClient.viewer;
+const getCurrentUser = async () => {
+  console.log(await linearClient.viewer);
+};
+
+async function getMyIssues() {
+  const me = await linearClient.viewer;
+  const myIssues = await me.assignedIssues();
+  console.log(myIssues);
 }
 
-(async ()=>{
-    const currentUser = await getCurrentUser()
-    console.log(currentUser)
-})()
+(async () => {
+  await getMyIssues();
+})();
